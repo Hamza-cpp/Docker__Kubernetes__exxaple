4f79938561dfa643b382cf21144f51e57dc6c576
hamza-cpp <hamza.okhair2018@gmail.com>
2023-05-07 18:17:11 +0100

    run in a Docker container

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello Docker World');
 });
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
-
-
-
-
-
-
-
-
-
 
 
