ba368bbf2703e68d47b979e10f398d8150ed92bf
hamza-cpp <hamza.okhair2018@gmail.com>
2023-05-07 21:00:28 +0100

    run in a kubernetes cluster

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Docker World');
+    res.send('Hello Kubernetes World');
 });
 
 app.listen(3000, () => {
